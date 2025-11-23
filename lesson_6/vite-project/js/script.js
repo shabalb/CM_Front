71ac095219389a385c1674b2317948eda170b491
shabalb <balyshevpavel71@gmail.com>
2025-11-23 19:38:42 +0300

    feat: add smoth scroll

---
+++ lesson_6/vite-project/js/script.js
@@ -0,0 +1,22 @@
+console.log('Работает!');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('a[href^="#"]');
+  
+  links.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+        targetElement.style.backgroundColor = '#3f3f408e';
+      }
+    });
+  });
+});
