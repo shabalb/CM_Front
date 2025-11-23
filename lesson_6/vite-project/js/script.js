ae58367436df8a91a65d34e830b3cb94f8d9980e
shabalb <balyshevpavel71@gmail.com>
2025-11-23 20:55:31 +0300

    no message

--- lesson_6/vite-project/js/script.js
+++ lesson_6/vite-project/js/script.js
@@ -1,22 +1,55 @@
 console.log('Работает!');
 
 document.addEventListener('DOMContentLoaded', () => {
-  const links = document.querySelectorAll('a[href^="#"]');
-  
-  links.forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-      
-      if (targetElement) {
-        targetElement.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            document.documentElement.style.scrollPaddingTop = '60px';
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+
+            }
         });
-        targetElement.style.backgroundColor = '#3f3f408e';
-      }
     });
-  });
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('li');
+    const getVisibleSection = () => {
+        let currentSection = '';
+        const scrollY = window.pageYOffset;
+
+        sections.forEach(section => {
+            const sectionHeight = section.offsetHeight;
+            const sectionTop = section.offsetTop - 100;
+            const sectionId = section.getAttribute('id');
+
+            if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+                currentSection = sectionId;
+            }
+        });
+
+        return currentSection;
+    };
+
+
+    const updateActiveLink = () => {
+        const currentSection = getVisibleSection();
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === `#${currentSection}`) {
+                link.classList.add('active');
+            }
+        });
+    };
+
+    window.addEventListener('scroll', updateActiveLink);
+    updateActiveLink();
 });
