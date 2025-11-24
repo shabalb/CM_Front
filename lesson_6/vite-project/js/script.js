aa7ce7dc0bc0cad715ed6b320a1872c043bae037
shabalb <balyshevpavel71@gmail.com>
2025-11-24 23:37:37 +0300

    feat: save beore changing

--- lesson_6/vite-project/js/script.js
+++ lesson_6/vite-project/js/script.js
@@ -52,4 +52,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', updateActiveLink);
     updateActiveLink();
+
+
+
+    
 });
+const navExpand = [].slice.call(document.querySelectorAll('.nav-expand'))
+    const backLink = `<li class="nav-item">
+  <a class="nav-link nav-back-link" href="javascript:;">
+    Back
+  </a>
+</li>`
+
+    navExpand.forEach(item => {
+        item.querySelector('.nav-expand-content')?.insertAdjacentHTML('afterbegin', backLink)
+        item.querySelector('.nav-link').addEventListener('click', () => item.classList.add('active'))
+        item.querySelector('.nav-back-link')?.addEventListener('click', () => item.classList.remove('active'))
+    })
+
+
+    // ---------------------------------------
+    // not-so-important stuff starts here
+
+    const ham = document.getElementById('ham')
+    ham.addEventListener('click', function () {
+        document.body.classList.toggle('nav-is-toggled')
+    })
